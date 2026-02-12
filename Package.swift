b0d3f47b78489f73acc7111d0b8f4355cf51e734
jorge-arionkoder <joan.mazo@kenility.com>
2026-02-12 17:19:23 -0500

    Update 0.0.4

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkMediaProcessing",
-            url: "https://github.com/joan-mazo-kenility/TruvideoMediaProcessing/releases/download/0.0.3/TruvideoSdkMediaProcessing.xcframework.zip",
+            url: "https://github.com/joan-mazo-kenility/TruvideoMediaProcessing/releases/download/0.0.4/TruvideoSdkMediaProcessing.xcframework.zip",
             checksum: "677f1879b5a9898ea4c38509364706d0408ae7c6e0f08b7a7f5983578ff052f4"
         )
     ]
